64b4e413b0b838f86e2cced7dff8cb5ff6ea073e
dabruge <davidbrucegeddes@gmail.com>
2023-09-03 18:34:14 +0100

    blackjack.py: Create hand class

--- blackjack.py
+++ blackjack.py
@@ -19,3 +19,49 @@ class Deck:
     def deal(self):
         if len(self.cards) > 1:
             return self.cards.pop(0)
+        
+class Hand:
+    def __init__(self, dealer=False):
+        self.dealer = dealer
+        self.cards = []
+        self.value = 0
+
+    def add_card(self, card):
+        self.cards.append(card)
+
+    def calculate_value(self):
+        self.value = 0
+        has_ace = False
+        for card in self.cards:
+            if card.value.isnumeric():
+                self.value += int(card.value)
+            else:
+                if card.value == "Ace":
+                    has_ace = True
+                    self.value += 11
+                else:
+                    self.value += 10
+        if has_ace and self.value > 21:
+            self.value -= 10
+    
+    def get_value(self):
+        self.calculate_value()
+        return self.value
+    
+    def display(self):
+        if self.dealer:
+            print("Hidden")
+            print(self.cards[1])
+        else:
+            for card in self.cards:
+                print(card)
+            print("Value: ", self.get_value())
+
+if __name__ == "__main__":
+    deck = Deck()
+    for card in deck.cards:
+        print(card.get_card_name())
+    deck.shuffle()
+    for card in deck.cards:
+        print(card.get_card_name())
+    
