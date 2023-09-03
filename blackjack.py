5c9c7269cdd87263cb57fcee8a56c330646db120
dabruge <davidbrucegeddes@gmail.com>
2023-09-03 18:08:53 +0100

    blackjack.py: Create deck class

--- blackjack.py
+++ blackjack.py
@@ -1,3 +1,5 @@
+import random
+
 class Card:
     def __init__(self, suit, value):
         self.suit = suit
@@ -5,3 +7,15 @@ class Card:
     
     def get_card_name(self):
         return f"{self.value} of {self.suit}"
+    
+class Deck:
+    def __init__(self):
+        self.cards = [Card(suit, value) for suit in ["Clubs", "Diamonds", "Hearts", "Spades"] for value in ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]]
+
+    def shuffle(self):
+        if len(self.cards) > 1:
+            random.shuffle(self.cards)
+
+    def deal(self):
+        if len(self.cards) > 1:
+            return self.cards.pop(0)
