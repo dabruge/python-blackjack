b47c7eed3a1785656f9e888193d2f9f887141d1d
dabruge <davidbrucegeddes@gmail.com>
2023-09-05 17:34:23 +0100

    gui-blackjack.py: Add GameState class

--- gui-blackjack.py
+++ gui-blackjack.py
@@ -28,3 +28,74 @@ class Deck:
     def deal(self):
         if len(self.cards) > 1:
             return self.cards.pop(0)
+
+class Hand:
+    def __init__(self, dealer=False):
+        self.dealer = dealer
+        self.cards = []
+        self.value = 0
+
+    def add_card(self, card):
+        self.cards.append(card)
+
+    def calculate_value(self):
+        self.value = 0
+        has_ace = False
+        for card in self.cards:
+            if card.value.isnumeric():
+                self.value += int(card.value)
+            else:
+                if card.value == "Ace":
+                    has_ace = True
+                    self.value += 11
+                else:
+                    self.value += 10
+        if has_ace and self.value > 21:
+            self.value -= 10
+    
+    def get_value(self):
+        self.calculate_value()
+        return self.value
+    
+class GameState:
+    def __init__(self):
+        self.deck = Deck()
+        self.deck.shuffle()
+
+        self.player_hand = Hand()
+        self.dealer_hand = Hand(dealer=True)
+
+        for i in range(2):
+            self.player_hand.add_card(self.deck.deal())
+            self.dealer_hand.add_card(self.deck.deal())
+        
+        self.has_winner = ''
+    
+    def player_is_over(self):
+        return self.player_hand.get_value() > 21
+    
+    def bj_occurred(self):
+        player = False
+        dealer = False
+        if self.player_hand.get_value() == 21:
+            player = True
+        if self.dealer_hand.get_value() == 21:
+            dealer = True
+        
+        if player and dealer:
+            return 'both'
+        elif player:
+            return 'player'
+        elif dealer:
+            return 'dealer'
+        
+        return False
+    
+    def hit(self):
+        self.player_hand.add_card(self.deck.deal())
+        if self.bj_occurred() == 'player':
+            self.has_winner = 'player'
+        if self.player_is_over():
+            self.has_winner = 'dealer'
+
+        return self.has_winner
